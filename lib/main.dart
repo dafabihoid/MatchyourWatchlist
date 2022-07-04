4fdb22bc25339656bcb91a18fee94dd5c0e7812a
Fabian <fabian.weiss@sz-ybbs.ac.at>
2022-07-04 18:02:16 +0200

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Homepage'),
+      home: const MyHomePage(title: 'Test1'),
     );
   }
 }

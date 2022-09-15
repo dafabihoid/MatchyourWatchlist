495182b2512375ca3492411b49d9df6a2d07e532
Erik Wagner <erik.wagne@sz-ybbs.ac.at>
2022-09-15 16:01:03 +0200

    Filter Page first steps

--- lib/pages/FilterPage.dart
+++ lib/pages/FilterPage.dart
@@ -9,7 +9,7 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
-
+  final List<Item> _data = generateItems(3);
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +21,71 @@ class _FilterPageState extends State<FilterPage> {
       ),
       backgroundColor: Colors.white,
       body: Container(
-
-        ));
-
-
+        padding: EdgeInsets.all(10),
+        child: Column (
+          children: [
+            _buildPanel(),
+          ],
+        ),
+      ),
+    );
   }
 
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expandedHeaderPadding: EdgeInsets.all(0),
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return Column(
+              children: [
+                ListTile(
+                  title: Text(item.headerValue),
+                ),
+              ]
+            );
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              subtitle:
+              const Text('To delete this panel, tap the trash can icon'),
+              trailing: const Icon(Icons.delete),
+              onTap: () {
+                setState(() {
+                  _data.removeWhere((Item currentItem) => item == currentItem);
+                });
+              }),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );
+  }
+}
+
+class Item {
+  Item({
+    required this.expandedValue,
+    required this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  List<String> headers = ["Genre", "Anbieter", "Sprachen"];
+
+  return List<Item>.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: headers[index],
+      expandedValue: 'This is item number $index',
+    );
+  });
 }

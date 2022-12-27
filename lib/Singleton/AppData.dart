6e2c78a1c8ce1af59979431aeaba61efdf95f6f9
Erik Wagner <erik.wagne@sz-ybbs.ac.at>
2022-12-27 11:43:57 +0100

    ZWC

--- lib/Singleton/AppData.dart
+++ lib/Singleton/AppData.dart
@@ -2,6 +2,7 @@ class AppData {
   static final AppData _appData = AppData._internal();
 
   bool filterSettingsAreAvailable = false;
+  String appLanguage = "de-de";
 
   factory AppData(){
     return _appData;
